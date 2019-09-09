a8c209944418b3fc096af563544f99b7826b4a2e
Test Example <test@example.org>
2019-09-09 19:29:04 +0500

    Микрофикс

--- gluttony/wsgi.py
+++ gluttony/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gluttony.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gluttony.settings.common')
 
 application = get_wsgi_application()
